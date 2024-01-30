47ee31dfe039541358073a67f1186a8cc35a3939
salehmb1999 <salehmirbagheri78@gmail.com>
2024-01-30 05:38:44 +0330

    nav completed & responsived

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,20 @@
 const $ = document;
 const mainLine = $.querySelector(".mainLine");
-
+const mobileMenu = $.querySelector(".mobileMenu");
 let isMenuOpen = false;
 mainLine.addEventListener("click", () => {
   if (!isMenuOpen) {
     mainLine.classList.add("activeMenu");
+    mobileMenu.style.display = "flex";
     mainLine.style.backgroundColor = "transparent";
     isMenuOpen = true;
   } else {
     mainLine.classList.remove("activeMenu");
+    mobileMenu.style.display = "none";
+
     mainLine.style.backgroundColor = "#000";
 
     isMenuOpen = false;
   }
 });
+
